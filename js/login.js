09e4d1415e0f149c57a898dac394f620065f8088
pcncrekr1 <pcncrekr1@gmail.com>
2020-10-14 17:48:32 +0900

    로그인 화면 구현
    ID: test, Password: 1234가 아니면 로그인되지 않도록 해둠

---
+++ js/login.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $("#loginForm").submit(function() { 
+        if( $("#loginId").val() !== "test" || 
+            $("#loginPw").val() !== "1234" ) {
+                alert("아이디나 암호가 틀리거나 회원이 아닙니다.\n확인하시기 바랍니다.");
+                return false;
+        }
+    });
+
+});
