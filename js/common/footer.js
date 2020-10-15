479b3dd92dd4afac100f3f509887e88271c72ec0
pcncrekr1 <pcncrekr1@gmail.com>
2020-10-15 17:14:28 +0900

    공통부분 추가 및 수정
    1. 로그인 후 ../../html/branch/main.html 로 이동하게 수정
    2. 헤더, 푸터 공통부분으로 빼서 jquery load로 불러오게 함

---
+++ js/common/footer.js
@@ -0,0 +1,3 @@
+$(function(){
+    document.getElementById("footer").innerHTML='<object type="text/html" data="../../html/common/footer.html"></object>';
+});
---
+++ js/common/header.js
@@ -0,0 +1,3 @@
+$(function(){
+    document.getElementById("header").innerHTML='<object type="text/html" data="../../html/common/header.html"></object>';
+});
