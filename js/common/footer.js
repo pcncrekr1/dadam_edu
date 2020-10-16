d5ace4f2a08ec02f5f8baa35141f75f6c63196ec
pcncrekr1 <pcncrekr1@gmail.com>
2020-10-16 11:52:38 +0900

    메가메뉴 틀 만듬
    1. 메가메뉴 틀 만듬
    2. 불필요한 파일 삭제

--- js/common/footer.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-    document.getElementById("footer").innerHTML='<object type="text/html" data="../../html/common/footer.html"></object>';
-});
--- js/common/header.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-    document.getElementById("header").innerHTML='<object type="text/html" data="../../html/common/header.html"></object>';
-});
