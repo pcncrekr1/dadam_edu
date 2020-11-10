8cb224da897f6720eb8841b4765224e302434e24
pcncrekr1 <pcncrekr1@gmail.com>
2020-11-10 09:51:20 +0900

    로그인 화면 링크 수정 및 스크립트 수정
    로그인 화면 링크 수정 및 스크립트 수정

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
 $(function(){
 
     $("#loginForm").submit(function() { 
-        if( $("#loginId").val() !== "test" || 
-            $("#loginPw").val() !== "1234" ) {
-                alert("아이디나 암호가 틀리거나 회원이 아닙니다.\n확인하시기 바랍니다.");
-                return false;
+        if( $("#loginId").val() === "test" && 
+            $("#loginPw").val() === "1234") {
+            alert("로그인 되었습니다.");
+        } else {
+            alert("아이디나 암호가 틀리거나 회원이 아닙니다.\n확인하시기 바랍니다.");
+            return false;
         }
     });
 
