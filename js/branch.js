1ec1cabfb796c6d74e4910afe4c6c60bdb45f039
pcncrekr1 <pcncrekr1@gmail.com>
2020-11-30 13:28:17 +0900

    폴더구조 정리 및 지사페이지-주문하기 메뉴 작업중
    폴더구조 정리 및 지사페이지-주문하기 메뉴 작업중

--- js/branch.js
+++ js/branch.js
@@ -161,10 +161,31 @@ $(function(){
         }
     });
 
+    // 장바구니 담기 버튼 클릭 시 선택된 상품이 없다면 알럿창 띄우기
+    $("#basketBtn").click(function () { 
+        if($("input:checkbox[name='book_order_check']:checked").length === 0){
+            alert("상품을 선택해 주세요.");
+            return false;
+        } else if( $("input:checkbox[name='book_order_check']:checked").length === 1 && 
+                   $("input:checkbox[name='book_order_check']:checked")[0].id === "bookOrderCheckAll") { //맨 위 체크박스만 선택된 경우
+                    alert("상품을 선택해 주세요.");
+                    return false;
+        }
+    });
+
+    // 바로 주문하기 클릭 시 상품이 없다면 알럿창 띄우기
+    $("#straightBtn").click(function () { 
+        if( $("#bookOrderEmptyTbody").children().length === 1 && 
+            $("#bookOrderEmptyTbody").children()[0].id === "bookOrderEmptyTr"){
+                alert("주문할 상품이 없습니다.");
+                return false;
+            }
+    });
 
 
 
-    // ------------ shopping_basket.html --------------
+
+    // ------------ a_s_request.html --------------
     // A/S접수 리스트 체크박스 컨트롤
     $(':checkbox[name=as_request_check]').change(function() {
         checkboxControl(this, "asRequestCheckAll", "as_request_check");
@@ -175,21 +196,61 @@ $(function(){
 
 
     // ------------ shopping_basket.html --------------
-    $("#choiceOrderBtn").click(function () { 
-        var yes = confirm("선택한 상품을 주문하시겠습니까?");
-        if(yes === true) {
-            window.location.href = "/dadam_edu/html/branch/order/shopping_basket_choice.html";
-        }
+    // 장바구니 리스트 체크박스 컨트롤
+    $(':checkbox[name=shop_basket_check]').change(function() {
+        checkboxControl(this, "shopBasketCheckAll", "shop_basket_check");
+    });
 
-        event.preventDefault();
+    $("#choiceOrderBtn").click(function () { 
+        if($("input:checkbox[name='shop_basket_check']:checked").length === 0){
+            alert("상품을 선택해 주세요.");
+            return false;
+        } else if( $("input:checkbox[name='shop_basket_check']:checked").length === 1 && 
+                   $("input:checkbox[name='shop_basket_check']:checked")[0].id === "shopBasketCheckAll") { //맨 위 체크박스만 선택된 경우(상품이 없을때)
+                    alert("상품이 없습니다.");
+                    return false;
+        } else {
+            var yes = confirm("선택한 상품을 주문하시겠습니까?");
+            if(yes === false) {
+                return false;
+            }
+        }
     });
     $("#allOrderBtn").click(function () { 
-        var yes = confirm("전체 상품을 주문하시겠습니까?");
-        if(yes === true) {
-            window.location.href = "/dadam_edu/html/branch/order/shopping_basket_all.html";
+        if($("input:checkbox[name='shop_basket_check']:checked").length === 0){
+            alert("전체 상품을 선택해 주세요.");
+            return false;
+        } else if( $("input:checkbox[name='shop_basket_check']:checked").length === 1 && 
+                   $("input:checkbox[name='shop_basket_check']:checked")[0].id === "shopBasketCheckAll") { //맨 위 체크박스만 선택된 경우(상품이 없을떄)
+                    alert("상품이 없습니다.");
+                    return false;
+        } else if($("input:checkbox[name='shop_basket_check']").length === $("input:checkbox[name='shop_basket_check']:checked").length) { // 전체 체크박스가 체크되었을 때
+            var yes = confirm("전체 상품을 주문하시겠습니까?");
+            if(yes === false) {
+                return false;
+            }
+        } else {
+            alert("전체 상품을 선택해 주세요.");
+            return false;
         }
+    });
 
-        event.preventDefault();
+    // 장바구니 리스트 삭제 버튼 클릭시 행 삭제
+    $(document).on("click", ".shop_basket_del a", function(){
+
+        // 합계에서 소계를 뺀 뒤 행을 삭제한다.
+        // var bookPriceAll = removeComma($("#bookPriceAll").text()) - removeComma($(this).parent().prev().text());
+        // $("#bookPriceAll").text(addComma(bookPriceAll));
+        // $(this).parent().parent("tr").remove();
+
+        // tbody에 행이 하나도 없으면 빈 행을 넣어준다.
+        // if($("#shopBasketTbody").children("tr").length === 0){
+        //     var tr = '';
+        //     tr += '<tr id="bookOrderEmptyTr">';
+        //     tr +=   '<td colspan="10" class="empty_td">주문한 상품이 없습니다.</td>';
+        //     tr += '</tr>';
+        //     $("#shopBasketTbody").prepend(tr);
+        // }
     });
 
 });
